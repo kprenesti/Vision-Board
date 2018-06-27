389d9ba5fd66071d3a9798799eb9c4d471214ad6
Krista Prenesti <kprenesti@gmail.com>
2018-06-27 13:03:51 -0400

    Fixed a lingering merge conflict

--- client/src/VisionBoard.js
+++ client/src/VisionBoard.js
@@ -5,25 +5,16 @@ class VisionBoard extends Component {
     constructor(props){
         super(props);
         this.state = {
-<<<<<<< HEAD
-            data: []
-=======
             goalTypes: ['personal', 'professional', 'material', 'environmental']
->>>>>>> PanelStyles
         }
     }
 
     render(){
         return (
             <div>
-<<<<<<< HEAD
-                <h1>This is the Vision Board Component</h1>
-                <VisionPanel />
-=======
                 {this.state.goalTypes.map((g) =>{
                    return <VisionPanel goal={g} active={false}/>
                 })}                
->>>>>>> PanelStyles
             </div>
         );
     }
