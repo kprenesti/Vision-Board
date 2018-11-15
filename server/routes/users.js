2c83d138337cb88c9b379e02f206f46ed0a339a5
Krista Prenesti <kprenesti@gmail.com>
2018-11-14 20:46:45 -0500

    Created the Users crud routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,29 +6,30 @@ const User = require('../models/User');
 
 
 //Create a new User
-router.post('/', (req, res, next)=>{
-  let body = req.body;
-  let date = Date.now();
-  const user = new User(Object.assign({}, body, {dateCreated: date, dateUpdated: date}));
-  User.find({username: body.username}).then((theUser)=>{
-    if(theUser.username === body.username){
-      // res.status(400).json({"error": "The username is already in use.  Please try again."});
-      // throw new Error('The username is already in use.');
-      console.log('This works.')
-      return;
-      console.log(theUser)
-    } //end if
-    else {
-      console.log("inElse", theUser)
-      // user.save().then((response)=>{
-      //   if(response){
-      //     res.status(200).json({"response": "The user was saved successfully!"});
-        }
-      }).catch((err)=> {
-        res.status(400).json(err)
-      }) //end .catch ; //end user.save.then
-     //end else
-}); //end router.post
+router.post('/', (req, res, next) => {
+      let body = req.body;
+      let date = Date.now();
+      const user = new User(Object.assign({}, body, {
+        dateCreated: date,
+        dateUpdated: date
+      }));
+      User.find({username: body.username})
+      .then((theUser) => {
+        if (theUser[0].username === body.username) {
+          // res.status(400).json({"error": "The username is already in use.  Please try again."});
+          throw new Error('The username is already in use.');
+        } //end if
+        else {
+          user.save().then((response) => {
+            if (response) {
+              res.status(200).json({
+                "response": "The user was saved successfully!"
+              });
+            }
+          })
+        } //end else
+      }).catch(next) //end .catch ; //end user.save.then
+      }); //end router.post
 
 
 //Get a list of all users
@@ -43,13 +44,24 @@ router.get('/', (req, res, next)=>{
 })
 
 //Retrieve a specific user by Username, which must be unique.
-router.get('/:username', function(req, res, next) {
+router.get('/:username', (req, res, next) => {
   User.find({username: req.params.username}).then((user)=>{
     if(user){
       res.status(200).json(user);
     }
-  }).catch(err => res.status(404).json(err))
+  }).catch(next)
 });
 
+
+//Modify an existing User
+router.put('/:username', (req, res, next)=>{
+  let updatedInfo = {...req.body, dateUpdated: Date.now()}
+  User.findOneAndUpdate({username: req.params.username}, updatedInfo, (err)=> {
+    if(err){
+      res.status(500).json(err)
+    }  
+  }).then((updatedUser)=>{res.status(200).json(updatedUser)});
+  })
+});
 
 module.exports = router;
