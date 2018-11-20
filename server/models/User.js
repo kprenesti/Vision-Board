c6ffaddbca1b7c49cde726f2271f307585935f00
Krista Prenesti <kprenesti@gmail.com>
2018-11-19 20:36:28 -0500

    Updated User Schema to incorporate goals, resources, and tasks.

--- server/models/User.js
+++ server/models/User.js
@@ -2,25 +2,59 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    dateCreated: Date,
-    dateUpdated: {
-        type: Date,
-        default: Date.now
-    },
-    goals: [{type: Schema.Types.ObjectId, ref: 'Goal'}]
+	first_name: {
+		type: String,
+		required: true
+	},
+	last_name: {
+		type: String
+	},
+
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	dateCreated: Date,
+	dateUpdated: {
+		type: Date,
+		default: Date.now
+	},
+	goals: [
+		{
+			name: {
+				type: String,
+				required: true
+			},
+			dateCreated: Date,
+			dateUpdated: {
+				type: Date,
+				default: Date.now
+			},
+			description: {
+				type: String,
+				required: true
+			},
+			accomplished: Boolean,
+			tasks: [{
+				name: {type: String, required: true},
+				description: String,
+				completed: Boolean
+			}],
+			resources: [
+				{
+					name: String,
+					url: String,
+					description: String
+				}
+			]
+			coverPhoto: String
+		}
+	]
 });
 
 const User = mongoose.model('user', UserSchema);
